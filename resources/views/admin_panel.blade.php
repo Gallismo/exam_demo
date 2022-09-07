dbda6a948a9168a6decb3605feacbafa0d544054
gosha <galilovgeorgij@gmail.com>
2022-09-07 17:36:10 +0300

    almaz-l**

---
+++ resources/views/admin_panel.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+<div class="add_video">
+    <a href="/add_video" class="button_add_videos">+</a>
+    <a href="/logout" class="bt_log">logout</a>
+</div>
+<div class="container">
+
+    <a href="/view_video" class="vd">
+        <video class="vd">
+        </video>
+    </a>
+
+    <h1 class="vd_name">
+        ЭТО НОЗВАНИЯ РОЛИКА ОНИ ДОЛЖНЫ СОРТИРОВАТСЯ ПО ДАТЕ
+    </h1>
+    <div class="dsp_vid">
+        <h3 class="categories_vid">для детей \категория </h3>
+        <h3 class="data_vid">11:10 01.09.2022</h3>
+        <h3 class="like_vid">5 лайки</h3>
+        <h3 class="dislike_vid">2 дизлайки </h3>
+        <h3 class="description_vid">Детский мультик созданный для детей с целью просмотра мультика детьми(Описание) </h3>
+        <h3 class="limitation_vid">ограничение видео</h3>
+    </div>
+</div>
+</body>
+</html>
